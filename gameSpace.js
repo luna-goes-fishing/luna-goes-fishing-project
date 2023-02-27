aaebd62b95c564f9880584acfcfa8b983748234a
professionaljared <misterjrd22@gmail.com>
2023-02-26 22:04:30 -0500

    added movement, testing shooting mechanics

--- gameSpace.js
+++ gameSpace.js
@@ -2,44 +2,77 @@
 import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
 // import {cake} from "./jaredScript.js"
 // initialize kaboom context
+let userScore = 0
+let timeLeft = 60
 
-kaboom()
+kaboom({
+	width:1000,
+	height:1500
+})
 // Load assets
 loadSprite("boat", "./game-assets/boat.png")
 
 // Define player movement speed (pixels per second)
-const SPEED = 320
+const SPEED = 500
 
 // Add player game object
 const player = add([
 	sprite("boat"),
-	// center() returns the center point vec2(width() / 2, height() / 2)
-	pos(width() / 2, height() / 2),
-    scale(0.4)
+	pos(400, 300),
+    scale(0.4),
+	solid(),
+	area()
 ])
 
 // onKeyDown() registers an event that runs every frame as long as user is holding a certain key
 onKeyDown("left", () => {
-	// .move() is provided by pos() component, move by pixels per second
-	player.move(-SPEED, 0)
+	if(player.pos.x > 1){
+		player.move(-SPEED, 0);
+	}
 })
 
 onKeyDown("right", () => {
-	player.move(SPEED, 0)
+	if(player.pos.x < 790){
+		player.move(SPEED, 0);
+	}
 })
 
 onKeyDown("down", () => {
-	console.log("hello")
+	console.log(player.pos.x);
 })
 
-// onClick() registers an event that runs once when left mouse is clicked
-// onClick(() => {
-// 	// .moveTo() is provided by pos() component, changes the position
-// 	player.moveTo(mousePos())
-// })
+player.onUpdate(() => {
+	// .isHovering() is provided by area() component, which returns a boolean of if the object is currently being hovered on
+	if (player.isHovering()) {
+		player.color = rgb(0, 0, 255)
+	} else {
+		player.color = rgb()
+	}
+})
 
 add([
 	// text() component is similar to sprite() but renders text
-	text("Press arrow keys", { width: width() / 2 }),
+	text(`Score: ${userScore}`, { width: width() / 2 }),
 	pos(12, 12),
 ])
+const timer = add([
+	// text() component is similar to sprite() but renders text
+	text(`Time: ${timeLeft}`, { width: width() / 2 }),
+	pos(630, 12),
+	{userTimer: 45 }
+])
+timer.onUpdate(() => {
+	if(timer.userTimer >= 0){
+		timer.userTimer -= dt()
+		timer.text = `Time: ${timer.userTimer.toFixed()}`
+	}
+})
+player.onUpdate(() => {
+	// .isHovering() is provided by area() component, which returns a boolean of if the object is currently being hovered on
+	if (player.isHovering()) {
+		player.color = rgb(0, 0, 255)
+	} else {
+		player.color = rgb()
+	}
+})
+debug.inspect = true
--- jaredScript.js
+++ jaredScript.js
@@ -1,6 +1,6 @@
 const start = document.querySelector("#header");
 
-console.log(start)
+
 start.addEventListener("click",e => {
     location.href = "./game.html";
 })
