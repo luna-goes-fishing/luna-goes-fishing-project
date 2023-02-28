67fbd78620c85f217b21be5bfe945d8f3effa7d2
professionaljared <misterjrd22@gmail.com>
2023-02-28 14:10:19 -0500

    error on fish 1 load

--- ayaizScript.js
+++ ayaizScript.js
@@ -80,7 +80,61 @@ scene("game", () => {
     fixed()
   ])
   //where mathew's code is
+  loadSprite("fish", "./sprites/fish.png");
+  loadSprite("fishOne", "./sprites/fish.png");
+  loadSprite("fishTwo", "./sprites/fish.png");
+  loadSprite("fishThree", "./sprites/fish.png");
 
+  const SPEED = 350;
+  const speedOne = 100;
+
+  loop(rand(2.5, 8), () => {
+    add([sprite("fishOne"), scale(0.3, 0.29), pos(110, 450), "fish"]),
+      onUpdate("fishOne", (fOne) => {
+      //   fOne.move(100, 0);
+      });
+  });
+  loop(rand(2.5, 9), () => {
+    add([sprite("fishTwo"), scale(0.3, 0.29), pos(110, 550), "fish"]),
+      onUpdate("fishTwo", (fTwo) => {
+      //   fTwo.move(70, 0);
+      });
+  });
+  loop(rand(2.5, 10), () => {
+    add([sprite("fishThree"), scale(0.3, 0.29), pos(110, 650), "fish"]),
+      onUpdate("fishThree", (fThree) => {
+      //   fThree.move(0, 0);
+      });
+  });
+  onUpdate("fish", (fish) => {
+    fish.move(150, 0);
+    if (fish.pos.x > 1210) {
+      destroy(fish);
+    }
+  });
+
+  loadSprite("boat", "./sprites/boat.png");
+
+  const boat = add([
+    sprite("boat"),
+    pos(width() / 2, height() / 2),
+    origin("center"),
+    pos(center()),
+    scale(0.3, 0.3),
+    action("boat"),
+  ]);
+
+  onKeyDown("left", () => {
+    if (boat.pos.x > 200) {
+      boat.move(-SPEED, 0);
+    }
+  });
+
+  onKeyDown("right", () => {
+    if (boat.pos.x < 1235) {
+      boat.move(SPEED, 0);
+    }
+  });
   
 })
 
